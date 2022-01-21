b3310b65f0cf58fd86e6f7707c6b645a89a991b5
Yash Rawat <rawatyash1999@gmail.com>
2022-01-21 19:39:10 +0530

    chore(api): added morgan and body parser and handled errors in apis like 404

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 const productRoutes = require('./api/routes/products');
-app.use('/products', productRoutes);
+const orderRoutes = require('./api/routes/orders');
 
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 module.exports = app;
