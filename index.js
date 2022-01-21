66b06a53ef6fedc48164984a671b2a96faab2ef6
Yash Rawat <rawatyash1999@gmail.com>
2022-01-21 18:05:24 +0530

    removed everything

--- index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const returnedText = fs.readFileSync('./txt/file.txt', 'utf-8');
-console.log(returnedText);
-
-const textOut = `${returnedText} I am the boss.\nCreated on ${new Date()}`;
-fs.writeFileSync('./txt/out.txt', textOut);
