211a0d80fa62a015929d4a3d908c109533159cb5
Yash Rawat <rawatyash1999@gmail.com>
2022-01-23 19:38:10 +0530

    feat(product): added count and responses

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,14 @@ const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
   Product.find()
+    .select('name price _id')
     .exec()
     .then((docs) => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs,
+      };
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
